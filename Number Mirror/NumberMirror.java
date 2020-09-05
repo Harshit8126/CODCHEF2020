6b73b4f86f9841cfd720ba5446435d80e61e500c
HARSHIT VERMA <harshit.verma_cs18@gla.ac.in>
2020-09-05 11:41:08 +0530

    Update NumberMirror.java

--- Number Mirror/NumberMirror.java
+++ Number Mirror/NumberMirror.java
@@ -1,1 +1,21 @@
 
+/* package codechef; // don't place package name! */
+/* Code By : @harshitver31
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class codechef
+{
+    public static void main(String[] args) throws java.lang.Exception{
+        Scanner sc = new Scanner(System.in);
+        int a;
+        try{
+            a=sc.nextInt();
+            System.out.println(a);
+            
+        }
+        catch(Exception e){
+            System.out.println();
+        }
+    }
+}
