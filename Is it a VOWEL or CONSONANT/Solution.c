9dff83b7fcd3ca1134f30222933f05f9cde6e2a2
HARSHIT VERMA <harshit.verma_cs18@gla.ac.in>
2020-09-09 13:45:06 +0530

    final commit

--- Is it a VOWEL or CONSONANT/Solution.c
+++ Is it a VOWEL or CONSONANT/Solution.c
@@ -1,1 +1,20 @@
+/* 
+Code By : @harshitver31
+*/
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char C;
+	scanf("%c",&C);
+	if(C =='A' || C=='E'|| C=='I' || C=='O'|| C=='U')
+	{
+	    printf("Vowel");
+	}
+	else
+	{
+	    printf("Consonant");
+	}
+	return 0;
+}
 
