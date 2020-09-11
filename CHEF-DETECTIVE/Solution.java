b57d238d3a9e985763ef394ef7372b3fb3a96d60
HARSHIT VERMA <harshit.verma_cs18@gla.ac.in>
2020-09-11 15:10:34 +0530

    final commit

--- CHEF-DETECTIVE/Solution.java
+++ CHEF-DETECTIVE/Solution.java
@@ -2,3 +2,36 @@
 By:- @harshitver31
 */
 
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner( System.in);
+		try {
+		int n = sc.nextInt();
+	
+		int arr[] = new int[n+1];
+		Arrays.fill(arr,0);
+		for(int i = 1 ; i<=n;i++){
+		    int k = sc.nextInt();
+		    arr[k]+=1;
+		    
+		}
+		for(int i= 1; i<=n;i++)
+		    if(arr[i]==0)
+		    System.out.println(i+" ");
+		
+    	}
+    	catch(Exception e){
+	}
+	}
+	
+}
